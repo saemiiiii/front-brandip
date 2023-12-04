fee2e506f12b8778a3ce8946b5b1e59ccecaf00e
saemi <sh.kim@wowplanet.io>
2023-12-04 22:19:57 +0900

    결제 url 부분 수정

--- src/views/products/ProductShowView.vue
+++ src/views/products/ProductShowView.vue
@@ -113,7 +113,7 @@ export default {
       const queryString = Object.entries(data)
           .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
           .join('&');
-      window.location.href = `/ipay/ipay.html?${queryString}`;
+      window.location.href = `http://ec2-3-34-182-84.ap-northeast-2.compute.amazonaws.com:8080/ipay/ipay.html?${queryString}`;
       // window.location.replace(`/ipay.html?${queryString}`);
     },
     postPay() {
