8e21f615c04485b0914babc0781fa66b363dcbef
saemi <sh.kim@wowplanet.io>
2023-12-06 19:35:22 +0900

    결제마무으리

--- src/views/mypage/IpayView.vue
+++ src/views/mypage/IpayView.vue
@@ -127,7 +127,7 @@ export default {
 
 <template>
   <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-  <html>
+  <html style="display: none">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
