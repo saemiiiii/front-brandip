80b187c5e266ff2cf501f454193ee6f7a664e237
saemi <sh.kim@wowplanet.io>
2024-01-08 17:31:37 +0900

    qa수정 및 디자이너 변경

--- src/views/common/PrivacyTermsView.vue
+++ src/views/common/PrivacyTermsView.vue
@@ -1,14 +1,38 @@
-<script setup>
+<script>
+import axios from "axios";
 
+export default {
+  data() {
+    return {
+      terms: [],
+    }
+  },
+  mounted() {
+    this.termsDetail();
+  },
+  methods: {
+    termsDetail() {
+      axios.get(`v1/terms/PRVT0000`)
+          .then(res => {
+            this.terms = res.data.data;
+            console.log(this.terms);
+          })
+          .catch(err => {
+            console.error(err);
+          })
+    }
+  }
+}
 </script>
 
 <template>
   <v-app>
     <v-container>
-      <div class="mt-20">
+      <div class="mt-20 mb-20 pb-20">
         <p style="font-family: Inter;font-size: 25px;font-weight: 700;">
           개인정보처리방침
         </p>
+        <p v-html="terms.contents"></p>
       </div>
     </v-container>
   </v-app>
